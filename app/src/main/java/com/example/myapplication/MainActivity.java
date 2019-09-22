2ea72d50b5943be5fd5585d9879c5d854ee0bd73
zhangqi <zhangqihappyzq@163.com>
2019-09-22 17:16:24 +0800

    新增fileUtils工具列

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/MyApplication.java
@@ -0,0 +1,13 @@
+package com.example.myapplication;
+
+import android.app.Application;
+
+public  class MyApplication extends Application {
+
+
+
+//    public static MyApplication getInstance() {
+//
+//        return get;
+//    }
+}
